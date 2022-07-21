e8842045193d5043b74d31f9bdf7d1a3e6965a08
belyaev-ay <anton.belyaev@sibit.tech>
2022-07-21 19:07:08 +0700

    Chapter 2 Ex 1

--- src/main/java/pw/avvero/ctci/Chapter2Ex1.java
+++ src/main/java/pw/avvero/ctci/Chapter2Ex1.java
@@ -19,4 +19,25 @@ public class Chapter2Ex1 {
         }
         return origin;
     }
+
+    public static <T> Node<T> removeDuplicates2(Node<T> origin) {
+        Node<T> current = origin;
+        while (current.getNext() != null) {
+            Node<T> next = current.getNext();
+            if (!contains(origin, next)) {
+                current = next;
+            } else {
+                current.setNext(next.getNext());
+            }
+        }
+        return origin;
+    }
+
+    private static <T> boolean contains(Node<T> current, Node<T> entry) {
+        while (current != null && current != entry) {
+            if (current.getValue().equals(entry.getValue())) return true;
+            current = current.getNext();
+        }
+        return false;
+    }
 }
--- src/test/groovy/pw/avvero/ctci/Chapter2Ex1Tests.groovy
+++ src/test/groovy/pw/avvero/ctci/Chapter2Ex1Tests.groovy
@@ -17,4 +17,17 @@ class Chapter2Ex1Tests extends Specification {
         [2, 2, 2]             || [2]
         [1, 1, 2, 2, 3, 3, 4] || [1, 2, 3, 4]
     }
+
+    @Unroll
+    def "2: #list without duplicates is #result"() {
+        expect:
+        Chapter2Ex1.removeDuplicates2(Node.of(list)) == Node.of(result)
+        where:
+        list                  || result
+        []                    || []
+        [1, 2, 3]             || [1, 2, 3]
+        [1, 2, 2]             || [1, 2]
+        [2, 2, 2]             || [2]
+        [1, 1, 2, 2, 3, 3, 4] || [1, 2, 3, 4]
+    }
 }
