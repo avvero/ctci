8b77d4279cc86174196238ce4483f81045441517
belyaev-ay <anton.belyaev@sibit.tech>
2022-07-27 19:56:33 +0700

    Packages

--- src/main/java/pw/avvero/ctci/chapter2/Chapter2Ex3.java
+++ src/main/java/pw/avvero/ctci/chapter2/Chapter2Ex3.java
@@ -3,7 +3,15 @@ package pw.avvero.ctci.chapter2;
 public class Chapter2Ex3 {
 
     public static <T> Node<T> remove(Node<T> node, T value) {
-        return node;
+        Node<T> root = node;
+        Node<T> current = node;
+        while (current != null && current.getNext() != null) {
+            if (value.equals(current.getNext().getValue())) {
+                current.setNext(current.getNext().getNext());
+            }
+            current = current.getNext();
+        }
+        return root;
     }
 
 }
--- src/test/groovy/pw/avvero/ctci/chapter2/Chapter2Ex3Tests.groovy
+++ src/test/groovy/pw/avvero/ctci/chapter2/Chapter2Ex3Tests.groovy
@@ -12,12 +12,11 @@ class Chapter2Ex3Tests extends Specification {
         expect:
         Chapter2Ex3.remove(Node.of(list), element) == Node.of(result)
         where:
-        list         | element || result
-        [1]          | 1       || []
-        [1, 2]       | 1       || [2]
-        [1, 2, 3]    | 1       || [2, 3]
-        [1, 2, 3]    | 2       || [1, 3]
-        [1, 2, 3]    | 4       || [1, 2, 3]
-        [1, 2, 3, 3] | 2       || [1, 3]
+        list               | element || result
+        [1, 2, 3]          | 2       || [1, 3]
+        [1, 2, 3, 4, 5, 6] | 2       || [1, 3, 4, 5, 6]
+        [1, 2, 3, 4, 5, 6] | 3       || [1, 2, 4, 5, 6]
+        [1, 2, 3, 4, 5, 6] | 4       || [1, 2, 3, 5, 6]
+        [1, 2, 3, 4, 5, 6] | 5       || [1, 2, 3, 4, 6]
     }
 }
