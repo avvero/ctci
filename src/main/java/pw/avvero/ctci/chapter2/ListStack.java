5750624e31074f438403914e346a5b8d02d7fba2
belyaev-ay <anton.belyaev@sibit.tech>
2022-09-19 17:22:19 +0700

    Chapter3

---
+++ src/main/java/pw/avvero/ctci/chapter2/ListStack.java
@@ -0,0 +1,52 @@
+package pw.avvero.ctci.chapter2;
+
+public class ListStack<T> {
+
+    private T[] list;
+    private int capacity;
+    private int m;
+    private Integer[] positions;
+
+    public ListStack(int m) {
+        this.capacity = 100; //TODO
+        this.m = m;
+        this.list = (T[]) new Object[capacity];
+        this.positions = new Integer[m];
+    }
+
+    public void push(int i, T t) {
+        positions[i] = positions[i] != null ? positions[i] + m : i;
+        list[positions[i]] = t;
+    }
+
+    public T peek(int i) {
+        if (positions[i] == null) return null;
+
+        return list[positions[i]];
+    }
+
+    public T pop(int i) {
+        if (positions[i] == null) return null;
+
+        T value = list[positions[i]];
+        positions[i] = positions[i] - m;
+        if (positions[i] < 0) positions[i] = null;
+        return value;
+    }
+
+    public String toString() {
+        StringBuilder sbuilder = new StringBuilder("[");
+
+        int maxPosition = 0;
+        for (Integer position : positions) {
+            if (position == null) continue;
+            maxPosition = Math.max(position, maxPosition);
+        }
+
+        for (int i = 0; i < list.length && i <= maxPosition; i++) {
+            sbuilder.append(list[i]).append(",");
+        }
+        sbuilder.append("]");
+        return sbuilder.toString();
+    }
+}
---
+++ src/test/groovy/pw/avvero/ctci/chapter2/Chapter3Ex1Tests.groovy
@@ -0,0 +1,39 @@
+package pw.avvero.ctci.chapter2
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Chapter3Ex1Tests extends Specification {
+
+    @Unroll
+    def "1: #Stack implementation"() {
+        when:
+        def stack = new ListStack(3)
+        stack.push(0, "2")
+        stack.push(2, "12")
+        then:
+        stack.toString() == '[2,null,12,]'
+        stack.peek(0) == "2"
+        stack.peek(2) == "12"
+        when:
+        stack.push(0, "1")
+        stack.push(2, "11")
+        then:
+        stack.toString() == '[2,null,12,1,null,11,]'
+        stack.peek(0) == "1"
+        stack.peek(2) == "11"
+        and:
+        stack.pop(0) == "1"
+        stack.pop(0) == "2"
+        stack.pop(2) == "11"
+        stack.pop(2) == "12"
+        and:
+        stack.peek(0) == null
+        stack.peek(2) == null
+        when:
+        stack.push(0, "2")
+        stack.push(2, "12")
+        then:
+        stack.toString() == '[2,null,12,]'
+    }
+}
