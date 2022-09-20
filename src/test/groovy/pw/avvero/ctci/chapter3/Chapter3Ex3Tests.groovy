180f23c6f9a6ba077a61edf9d4f8b78c3ed4104f
belyaev-ay <anton.belyaev@sibit.tech>
2022-09-20 14:48:29 +0700

    Chapter3Ex3

---
+++ src/test/groovy/pw/avvero/ctci/chapter3/Chapter3Ex3Tests.groovy
@@ -0,0 +1,53 @@
+package pw.avvero.ctci.chapter3
+
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Chapter3Ex3Tests extends Specification {
+
+    @Unroll
+    def "1: #Stack implementation"() {
+        when:
+        def stack = new Stack()
+        stack.push("2")
+        then:
+        stack.peek() == "2"
+        stack.toString() == '[2]'
+        when:
+        stack.push("1")
+        then:
+        stack.peek() == "1"
+        stack.toString() == '[1, 2]'
+        when:
+        stack.push("3")
+        then:
+        stack.peek() == "3"
+        stack.toString() == '[3, 1, 2]'
+        when:
+        stack.push("4")
+        then:
+        stack.peek() == "4"
+        stack.toString() == '[4, 3, 1, 2]'
+        when:
+        stack.push("5")
+        then:
+        stack.peek() == "5"
+        stack.toString() == '[5, 4, 3, 1, 2]'
+        when:
+        stack.push("6")
+        then:
+        stack.peek() == "6"
+        stack.toString() == '[6, 5, 4, 3, 1, 2]'
+        when:
+        stack.pop() == "1"
+        stack.pop() == "2"
+        stack.pop() == "3"
+        stack.pop() == "4"
+        stack.pop() == "5"
+        stack.pop() == "6"
+        then:
+        stack.peek() == null
+        stack.toString() == '[]'
+    }
+}
