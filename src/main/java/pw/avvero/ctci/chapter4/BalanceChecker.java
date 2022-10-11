db22908a1be808782f0633db9030d708ac61121b
belyaev-ay <anton.belyaev@sibit.tech>
2022-10-11 19:10:39 +0700

    Chapter4Ex1

---
+++ src/main/java/pw/avvero/ctci/chapter4/BalanceChecker.java
@@ -0,0 +1,28 @@
+package pw.avvero.ctci.chapter4;
+
+import static pw.avvero.ctci.chapter4.BinaryTree.Node;
+
+public class BalanceChecker {
+
+    public static boolean isBalanced(BinaryTree tree) {
+        try {
+            height(tree.root);
+        } catch (RuntimeException re) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int height(Node node) {
+        if (node == null) return 0;
+
+        int lh = height(node.left) + 1;
+        int rh = height(node.right) + 1;
+
+        if (lh - rh > 1) throw new RuntimeException();
+        if (rh - lh > 1) throw new RuntimeException();
+
+        return Math.max(lh, rh);
+    }
+
+}
--- src/test/groovy/pw/avvero/ctci/chapter4/BinaryTreeTests.groovy
+++ src/test/groovy/pw/avvero/ctci/chapter4/BinaryTreeTests.groovy
@@ -11,12 +11,12 @@ class BinaryTreeTests extends Specification {
         when:
         println tree.prettyPrint()
         then:
-        1 == 1
+        BalanceChecker.isBalanced(tree) == balanced
         where:
         balanced | tree
         true     | BinaryTree.of(5, 3, 1, 4, 7, 6, 8)
         false    | BinaryTree.of(1, 2, 3, 4, 5, 6, 7)
-        false    | BinaryTree.of(new Node(new Node(new Node(3), 6, new Node(5)), 1, new Node(null, 3, new Node(7))))
+        true     | BinaryTree.of(new Node(new Node(new Node(3), 6, new Node(5)), 1, new Node(null, 3, new Node(7))))
         true     | BinaryTree.of(new Node(new Node(9), 3, new Node(new Node(15), 20, new Node(7))))
         false    | BinaryTree.of(new Node(new Node(new Node(new Node(4), 3, new Node(4)), 2, new Node(3)), 1, new Node(2)))
     }
