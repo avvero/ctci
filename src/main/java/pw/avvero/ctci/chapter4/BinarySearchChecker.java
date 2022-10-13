3f3d51f1c4583dd29157eb854aaf7d10abb65ac2
belyaev-ay <anton.belyaev@sibit.tech>
2022-10-13 19:30:39 +0700

    Chapter4Ex4

--- src/main/java/pw/avvero/ctci/chapter4/BinarySearchChecker.java
+++ src/main/java/pw/avvero/ctci/chapter4/BinarySearchChecker.java
@@ -1,18 +1,19 @@
 package pw.avvero.ctci.chapter4;
 
 public class BinarySearchChecker {
-    public static <T extends Comparable<T>> boolean check(BinaryTree<T> tree) {
-        return check(tree.root);
+    public static boolean check(BinaryTree<Integer> tree) {
+        return check(tree.root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
-    private static <T extends Comparable<T>> boolean check(BinaryTree.Node<T> root) {
+    private static boolean check(BinaryTree.Node<Integer> root, Integer min, Integer max) {
+        if (root.value < min || root.value > max) return false;
         if (root.left != null) {
             if (root.left.value.compareTo(root.value) >= 0) return false;
-            if (!check(root.left)) return false;
+            if (!check(root.left, min, root.value)) return false;
         }
         if (root.right != null) {
             if (root.right.value.compareTo(root.value) <= 0) return false;
-            if (!check(root.right)) return false;
+            if (!check(root.right, root.value, max)) return false;
         }
         return true;
     }
