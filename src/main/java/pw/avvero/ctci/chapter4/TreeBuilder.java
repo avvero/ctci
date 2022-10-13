b103d3d5927fa92e311a84fa4626786bc4ddaf56
belyaev-ay <anton.belyaev@sibit.tech>
2022-10-13 16:59:59 +0700

    Chapter4Ex3

--- src/main/java/pw/avvero/ctci/chapter4/TreeBuilder.java
+++ src/main/java/pw/avvero/ctci/chapter4/TreeBuilder.java
@@ -1,15 +1,26 @@
 package pw.avvero.ctci.chapter4;
 
-import java.util.List;
+import pw.avvero.ctci.chapter4.BinaryTree.Node;
 
 public class TreeBuilder {
 
-    public static <T extends Comparable<T>> BinaryTree<T> fromOrderedList(List<T> values) {
+    public static <T extends Comparable<T>> BinaryTree<T> fromOrderedList(T[] values) {
         BinaryTree<T> tree = new BinaryTree<>();
-        for (T node : values) {
-            tree.add(node);
-        }
+        tree.root = getNodes(values, 0, values.length - 1);
         return tree;
+    }
+
+    private static <T extends Comparable<T>> Node<T> getNodes(T[] values, int start, int end) {
+        int pivot = start + (end - start) / 2;
+        if (pivot < 0) return null;
+        Node<T> root = new Node<T>(values[pivot]);
+        if (start < pivot) {
+            root.left = getNodes(values, start, pivot - 1);
+        }
+        if (pivot < end) {
+            root.right = getNodes(values, pivot + 1, end);
+        }
+        return root;
     }
 
 }
--- src/test/groovy/pw/avvero/ctci/chapter4/Chapter4Ex3Tests.groovy
+++ src/test/groovy/pw/avvero/ctci/chapter4/Chapter4Ex3Tests.groovy
@@ -9,14 +9,17 @@ class Chapter4Ex3Tests extends Specification {
     @Unroll
     def "Tree from #list is #treeString"() {
         when:
-        def tree = TreeBuilder.fromOrderedList(list)
+        def tree = TreeBuilder.fromOrderedList(list as Integer[])
         println tree.prettyPrint()
         then:
         tree.toString() == treeString
         where:
-        list      | treeString
-        [1]       | '[_, 1, _]'
-        [1, 2, 3] | '[[_, 1, _], 2, [_, 3, _]]'
+        list                            | treeString
+        [1]                             | '[_, 1, _]'
+        [1, 2, 3]                       | '[[_, 1, _], 2, [_, 3, _]]'
+        [1, 2, 3, 4, 5]                 | '[[_, 1, [_, 2, _]], 3, [_, 4, [_, 5, _]]]'
+        [1, 2, 3, 4, 5, 7, 8]           | '[[[_, 1, _], 2, [_, 3, _]], 4, [[_, 5, _], 7, [_, 8, _]]]'
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] | '[[[_, 1, _], 2, [_, 3, [_, 4, _]]], 5, [[_, 6, [_, 7, _]], 8, [_, 9, [_, 10, _]]]]'
     }
 
 
