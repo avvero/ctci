1168a9d8c4d5acd9c1d087d7de7571a889d22be4
belyaev-ay <anton.belyaev@sibit.tech>
2023-02-02 11:58:20 +0700

    fix

---
+++ src/main/java/pw/avvero/leet/Problem69.java
@@ -0,0 +1,17 @@
+package pw.avvero.leet;
+
+public class Problem69 {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        int y = x;
+        int z = (y + x/y) / 2;
+        while (z < 0) {
+            z = 46340;
+        }
+        while (y - z > 0) {
+            y = z;
+            z = (y + x/y) / 2;
+        }
+        return z * z <= x ? z : z - 1;
+    }
+}
