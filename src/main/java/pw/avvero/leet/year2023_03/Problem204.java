d35edccb8fee29f38f3bc97c6374609959704cb3
belyaev-ay <anton.belyaev@sibit.tech>
2023-03-22 12:36:56 +0700

    fix

---
+++ src/main/java/pw/avvero/leet/year2023_03/Problem204.java
@@ -0,0 +1,28 @@
+package pw.avvero.leet.year2023_03;
+
+public class Problem204 {
+
+    public int countPrimes(int n) {
+        int[] primes = new int[5000000];
+        int primesi = 0;
+        int i = 2;
+        while (i < n) {
+            if (isPrime(i, primes, primesi)) {
+                primes[primesi++] = i;
+                i = i + (i < 7 ? 1 : 2);
+            } else {
+                i += 1;
+            }
+        }
+        return primesi;
+    }
+
+    private boolean isPrime(int n, int[] primes, int primesi) {
+        for (int i = 0; i < Math.min(primesi, primesi); i++) {
+            if (n % primes[i] == 0) {
+                return false;
+            };
+        }
+        return true;
+    }
+}
