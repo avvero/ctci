6b818d4a6c35ec69deef312925d18bd89dcb7f3a
belyaev-ay <anton.belyaev@sibit.tech>
2023-03-23 12:16:05 +0700

    fix

--- src/main/java/pw/avvero/leet/year2023_03/Problem215.java
+++ src/main/java/pw/avvero/leet/year2023_03/Problem215.java
@@ -5,26 +5,35 @@ import java.util.Arrays;
 public class Problem215 {
 
     public int findKthLargest(int[] nums, int k) {
-        int[] largest = new int[k];
-        Arrays.fill(largest, Integer.MIN_VALUE);
-        for (int i = 0; i < nums.length; i++) {
-            push(nums[i], largest, 0);
-        }
-        return largest[k - 1];
+        return select(nums, 0, nums.length - 1, k - 1);
     }
 
-    private void push(int max, int[] largest, int i) {
-        if (i == largest.length) return;
-        if (largest[largest.length - 1] > max) return;
-        if (max > largest[i]) {
-            int prev = largest[i];
-            largest[i] = max;
-            push(prev, largest, ++i);
+    private int select(int[] nums, int left, int right, int k) {
+        int pivotLoc = partition(nums, left, right);
+        if (pivotLoc == k) return nums[k];
+        if (pivotLoc < k) {
+            return select(nums, pivotLoc + 1, right, k);
         } else {
-            push(max, largest, ++i);
+            return select(nums, left, pivotLoc - 1, k);
         }
     }
 
-    // [3,2,1,5,6,4]
-    // [1,2,3,4,5,6]
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int pivotLoc = left;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] < pivot) {
+                int temp = nums[i];
+                nums[i] = nums[pivotLoc];
+                nums[pivotLoc] = temp;
+                pivotLoc++;
+            }
+        }
+        int temp = nums[right];
+        nums[right] = nums[pivotLoc];
+        nums[pivotLoc] = temp;
+        return pivotLoc;
+    }
+    //  7, 10, 4, 3, 20, 15
+    // 10,  7, 4, 3, 20, 15
 }
