e3722e77bbcb2b6f0006e394d4c27a395dc7e1cf
belyaev-ay <anton.belyaev@sibit.tech>
2023-06-19 14:15:53 +0700

    Fix

--- src/main/java/pw/avvero/leet/year2023_06/Problem76.java
+++ src/main/java/pw/avvero/leet/year2023_06/Problem76.java
@@ -10,65 +10,41 @@ public class Problem76 {
     // BANC
 
     public String minWindow(String s, String t) {
-        String result = "";
-        //
-        int[] targetHash = new int[64];
-        int targetCap = 0;
-        for (int i = 0; i < t.length(); i++) {
-            targetHash[hash(t.charAt(i))]++;
-            targetCap++;
+        if (t.length() > s.length()) {
+            return "";
         }
-        int[] hash = new int[64];
-        int cap = 0;
-        int i = 0, j = 0;
-        LinkedList<Integer> iz = new LinkedList<>();
-        while (j < s.length()) {
-            int h = hash(s.charAt(j));
-            if (targetHash[h] > 0) {
-                if (hash[h] < targetHash[h]) {
-                    cap++;
-                }
-                hash[h]++;
-                if (j > 0) {
-                    iz.add(j);
-                }
-            }
-            if (targetCap == cap) {
-                String nextResult = s.substring(i, j + 1);
-                if (nextResult.length() == t.length()) return nextResult;
-                if (result.equals("")  || nextResult.length() < result.length()) {
-                    result = nextResult;
-                }
-                int f = hash(s.charAt(i));
-                if (hash[f] > 0) {
-                    hash[f]--;
-                    if (hash[f] < targetHash[f]) {
-                        cap--;
-                        j++;
-                    } else { // first char is redundant, step back
-                        hash[h]--;
-                        cap--;
-                    }
-                } else { // first char is redundant, step back
-                    hash[h]--;
-                    cap--;
-                }
-                if (!iz.isEmpty()) {
-                    int nexti;
-                    do {
-                        nexti = iz.removeFirst();
-                    } while(i == nexti && !iz.isEmpty());
-                    i = nexti;
-                }
-            } else {
-                j++;
-            }
-        }
-        return result;
-    }
 
-    private int hash(char c) {
-        return c - 'A';
+        int l = 0;
+        int r = 0;
+        int index = 0;
+        int minLength = Integer.MAX_VALUE;
+        int[] map = new int[128];
+        int count = t.length();
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+
+        char[] sArray = s.toCharArray();
+        while (r < sArray.length) {
+            if (map[ sArray[r++] ]-- > 0) {
+                count--;
+            }
+            while (count == 0) {
+                if ((r - l) < minLength) {
+                    minLength = r - l;
+                    index = l;
+                }
+                if (map[ sArray[l++] ]++ == 0) {
+                    count++;
+                }
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE) {
+            return new String();
+        }
+
+        return new String(sArray, index, minLength);
     }
 
 }
