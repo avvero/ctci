aad033165e22fd9de42468770a8b521e11a54c2f
belyaev-ay <anton.belyaev@sibit.tech>
2023-07-20 12:06:34 +0700

    Fix

--- src/main/java/pw/avvero/leet/year2023_07/Problem224.java
+++ src/main/java/pw/avvero/leet/year2023_07/Problem224.java
@@ -5,66 +5,27 @@ import java.util.LinkedList;
 public class Problem224 {
 
     public int calculate(String s) {
-        long result = 0;
         LinkedList stack = new LinkedList();
-        LinkedList operand = new LinkedList();
-        long d = 0;
-        boolean dseq = false;
+
+        long result = 0;
+        long n = 0;
+        long sign = 1;
         char[] chars = s.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            char c = chars[i];
-            if (c == ' ') continue;
-            if (isDigit(c)) {
-                dseq = true;
-                d = d * 10 + c - '0';
-            } else {
-                if (dseq) {
-                    stack.add(d);
-                    d = 0;
-                    dseq = false;
-                }
-                //
-                if (operand.size() > 0) {
-                    if (c == ')') {
-                        char o = (Character) operand.peekLast();
-                        while (o != '(') {
-                            if (operand.size() > 0) {
-                                evalStack(stack, operand);
-                            }
-                            o = operand.peekLast() != null ? (Character) operand.peekLast() : 0;
-                        }
-                        operand.removeLast();
-                    } else if ((c == '+' || c == '-' ) && (Character) operand.peekLast() != '(') {
-                        evalStack(stack, operand);
-                    }
-                }
-                //
-                if (c != ')') {
-                    operand.add(c);
-                }
-            }
+           if (Character.isDigit(chars[i])) {
+               n = n * 10 + chars[i] - '0';
+           } else if (chars[i] == '+') {
+               result += n * sign;
+               n = 0;
+               sign = 1;
+           } else if (chars[i] == '-') {
+               result += n * sign;
+               n = 0;
+               sign = -1;
+           }
         }
-        if (dseq) {
-            stack.add(d);
-        }
-        if (operand.size() > 0) {
-            evalStack(stack, operand);
-        }
-        return ((Long) stack.removeLast()).intValue();
+        result += n * sign;
+        return (int )result;
     }
 
-    private void evalStack(LinkedList stack, LinkedList operand) {
-        long b = (long) stack.removeLast();
-        long a = (long) stack.removeLast();
-        char o = (Character) operand.removeLast();
-        if (o == '+') {
-            stack.add(a + b);
-        } else if (o == '-') {
-            stack.add(a - b);
-        }
-    }
-
-    private boolean isDigit(char c) {
-        return c >= '0' && c <= '9';
-    }
 }
--- src/test/groovy/pw/avvero/leet/year2023_07/Problem224Tests.groovy
+++ src/test/groovy/pw/avvero/leet/year2023_07/Problem224Tests.groovy
@@ -12,9 +12,9 @@ class Problem224Tests extends Specification {
         new Problem224().calculate(exp) == result
         where:
         exp                   | result
+        "1-(-2)"              | 3
         "1-(2+1)"             | -2
         "1 - 2 + 1"           | 0
-        "1-(-2)"              | 3
         "1-(     -2)"         | 3
         "-(2 + 3)"            | -5
         "1-(2+1+1)"           | -3
