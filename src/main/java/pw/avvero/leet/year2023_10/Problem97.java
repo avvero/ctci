b078f19360934bd7d2ec5e29304b3986fb02e9a5
belyaev-ay <anton.belyaev@sibit.tech>
2023-11-07 12:17:22 +0700

    Fix

--- src/main/java/pw/avvero/leet/year2023_10/Problem97.java
+++ src/main/java/pw/avvero/leet/year2023_10/Problem97.java
@@ -3,27 +3,51 @@ package pw.avvero.leet.year2023_10;
 public class Problem97 {
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        return isInterleave(s1.toCharArray(), 0, s2.toCharArray(), 0, s3.toCharArray(), 0);
-    }
-
-    public boolean isInterleave(char[] s1, int s1i, char[] s2, int s2i, char[] s3, int s3i) {
-        if (s1i == s1.length && s2i == s2.length && s3i == s3.length) return true;
-
-        if (s1i < s1.length && s2i < s2.length && s3i < s3.length &&
-                s1[s1i] == s3[s3i] && s2[s2i] == s3[s3i]) {
-            boolean result = isInterleave(s1, s1i, s2, s2i + 1, s3, s3i + 1);
-            if (result) {
-                return result;
-            } else {
-                return isInterleave(s1, s1i + 1, s2, s2i, s3, s3i + 1);
+        int m = s1.length();
+        int n = s2.length();
+        int t = s3.length();
+        if (m + n != t) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                int k = i + j - 1;
+                if (i > 0 && s1.charAt(i - 1) == s3.charAt(k)) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+                if (j > 0 && s2.charAt(j - 1) == s3.charAt(k)) {
+                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
             }
         }
-        if (s1i < s1.length && s3i < s3.length && s1[s1i] == s3[s3i]) {
-            return isInterleave(s1, s1i + 1, s2, s2i, s3, s3i + 1);
+        return dp[m][n];
+    }
+
+    public boolean isInterleave2(String s1, String s2, String s3) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        char[] c3 = s3.toCharArray();
+        int n = c1.length;
+        int m = c2.length;
+
+        if (n + m != c3.length) return false;
+
+        boolean[][] path = new boolean[n+1][m+1];
+        path[0][0] = true;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                int k = n + m - 1;
+                if (i > 0 && c3[k] == c1[i - 1]) {
+                    path[i][j] = path[i][j] || path[i - 1][j];
+                }
+                if (j > 0 && c3[k] == c2[j - 1]) {
+                    path[i][j] = path[i][j] || path[i][j-1];
+                }
+            }
         }
-        if (s2i < s2.length && s3i < s3.length && s2[s2i] == s3[s3i]) {
-            return isInterleave(s1, s1i, s2, s2i + 1, s3, s3i + 1);
-        }
-        return false;
+        return path[n][m];
     }
 }
---
+++ src/test/groovy/pw/avvero/leet/year2023_10/Problem123Tests.groovy
@@ -0,0 +1,18 @@
+package pw.avvero.leet.year2023_10
+
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Problem123Tests extends Specification {
+
+    @Unroll
+    def "test1"() {
+        expect:
+        new Problem123().maxProfit(prices as int[]) == result
+        where:
+        prices                   || result
+//        [3, 3, 5, 0, 0, 3, 1, 4] || 6
+        [3, 3, 5, 1, 1, 3, 1, 4] || 5
+    }
+}
