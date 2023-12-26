fc7b417b0ebdcf45e61956599150ede50cf2476d
belyaev-ay <anton.belyaev@sibit.tech>
2023-12-26 11:36:43 +0700

    Fix

--- src/test/groovy/pw/avvero/leet/year2023_12/Problem300Tests.groovy
+++ src/test/groovy/pw/avvero/leet/year2023_12/Problem300Tests.groovy
@@ -12,7 +12,7 @@ class Problem300Tests extends Specification {
         new Problem300().lengthOfLIS(nums as int[]) == result
         where:
         nums                         || result
-        [10, 9, 2, 5, 3, 7, 101, 18] || 4
+        [10, 9, 2, 5, 3, 7, 1, 101, 18] || 4
         [0, 1, 0, 3, 2, 3]           || 4
         [7, 7, 7, 7, 7, 7, 7]        || 1
     }
---
+++ src/test/groovy/pw/avvero/leet/year2023_12/Problem354Tests.groovy
@@ -0,0 +1,21 @@
+package pw.avvero.leet.year2023_12
+
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class Problem354Tests extends Specification {
+
+    @Unroll
+    def "test"() {
+        expect:
+        new Problem354().maxEnvelopes(envs as int[][]) == result
+        where:
+        envs                                                                                       || result
+        [[30, 50], [12, 2], [3, 4], [12, 15]]                                                      || 3
+        [[2, 100], [3, 200], [4, 300], [5, 500], [5, 400], [5, 250], [6, 370], [6, 360], [7, 380]] || 5
+        [[5, 4], [6, 4], [6, 7], [2, 3]]                                                           || 3
+        [[1, 1], [1, 1], [1, 1]]                                                                   || 1
+    }
+
+}
