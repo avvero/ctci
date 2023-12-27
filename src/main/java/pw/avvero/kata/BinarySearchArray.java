c6106e3e8c835804a3c44f6c1060752394909bef
belyaev-ay <anton.belyaev@sibit.tech>
2023-12-27 11:54:21 +0700

    Fix

--- src/main/java/pw/avvero/kata/BinarySearchArray.java
+++ src/main/java/pw/avvero/kata/BinarySearchArray.java
@@ -3,11 +3,24 @@ package pw.avvero.kata;
 public class BinarySearchArray {
 
     public int search(int[] arr, int target) {
-        return search(arr, 0, arr.length - 1, target);
+        return search(arr, 0, arr.length, target);
     }
 
     public int search(int[] arr, int left, int right, int target) {
-        return 0;
+        int low = left;
+        int high = right - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midValue = arr[mid];
+            if (midValue < target) {
+                low = mid + 1;
+            } else if (midValue > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
     }
 
 }
--- src/test/groovy/pw/avvero/kata/BinarySearchArrayTests.groovy
+++ src/test/groovy/pw/avvero/kata/BinarySearchArrayTests.groovy
@@ -13,6 +13,19 @@ class BinarySearchArrayTests extends Specification {
         where:
         arr                | target || result
         [1, 2, 3, 4, 5, 6] | 3      || 2
+        [1, 2, 3, 4, 5, 6] | 7      || -7
+        [1, 2, 3, 4, 5, 6] | 0      || -1
+    }
+
+    @Unroll
+    def "test2"() {
+        expect:
+        Arrays.binarySearch(arr as int[], 0, arr.size(), target) == result
+        where:
+        arr                | target || result
+        [1, 2, 3, 4, 5, 6] | 3      || 2
+        [1, 2, 3, 4, 5, 6] | 7      || -7
+        [1, 2, 3, 4, 5, 6] | 0      || -1
     }
 
 }
