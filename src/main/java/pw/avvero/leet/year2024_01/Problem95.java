d5d718dcd7319291cc9458e08b9b92a4f7f6b7c6
belyaev-ay <anton.belyaev@sibit.tech>
2024-01-31 11:48:56 +0700

    Fix

--- src/main/java/pw/avvero/leet/year2024_01/Problem95.java
+++ src/main/java/pw/avvero/leet/year2024_01/Problem95.java
@@ -44,51 +44,26 @@ public class Problem95 {
 
 
     public List<TreeNode> generateTrees(int n) {
-        List[] dp = new List[n + 1];
-        dp[0] = List.of();
-        dp[1] = List.of(new TreeNode(1));
-        for (int i = 2; i <= n; i++) {
-            dp[i] = new ArrayList<>();
-            for (int j = 1; j < i; j++) {
-                dp[i].addAll(multiply(dp[i - j], dp[j - 1]));
-            }
-            List<TreeNode> left = multiply(dp[i], List.of(new TreeNode(i)));
-            List<TreeNode> right = multiply(List.of(new TreeNode(i)), dp[i]);
-            dp[i] = new ArrayList();
-            dp[i].addAll(left);
-            dp[i].addAll(right);
-        }
-        return dp[n];
+        return subTrees(1, n);
     }
-
-    private List<TreeNode> multiply(List<TreeNode> as, List<TreeNode> bs) {
-        if (as.size() == 0) return bs;
-        if (bs.size() == 0) return as;
-
-        List<TreeNode> nodes = new ArrayList<>();
-        for (TreeNode a : as) {
-            for (TreeNode b : bs) {
-                nodes.add(populate(clone(a), clone(b)));
+    private List<TreeNode> subTrees(int start, int end){
+        List<TreeNode> res = new ArrayList();
+        if(start>end) {
+            res.add(null);
+            return res;
+        }
+        for(int i=start; i<=end; i++){
+            List<TreeNode> left = subTrees(start, i-1);
+            List<TreeNode> right = subTrees(i+1, end);
+            for(TreeNode l: left){
+                for(TreeNode r: right){
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
             }
         }
-        return nodes;
-    }
-
-    private TreeNode populate(TreeNode a, TreeNode b) {
-        if (a == null) return b;
-        if (a.val > b.val) {
-            a.left = populate(a.left, b);
-        } else {
-            a.right = populate(a.right, b);
-        }
-        return a;
-    }
-
-    private TreeNode clone(TreeNode node) {
-        if (node == null) return null;
-        TreeNode result = new TreeNode(node.val);
-        result.left = clone(node.left);
-        result.right = clone(node.right);
-        return result;
+        return res;
     }
 }
