5b68943ccb06bc32a6bd7f433615b5e88f9d3352
belyaev-ay <anton.belyaev@sibit.tech>
2024-03-19 12:01:14 +0700

    Fix

--- src/main/java/pw/avvero/leet/year2024_03/Problem1192.java
+++ src/main/java/pw/avvero/leet/year2024_03/Problem1192.java
@@ -1,35 +1,40 @@
 package pw.avvero.leet.year2024_03;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Problem1192 {
 
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int[][] path = new int[n][n];
+        int[] visited = new int[n];
+        int[] low = new int[n];
+        int[] desc = new int[n];
+        int id = 0;
         List<Integer>[] links = link(n, connections);
-        traverse(links, 0, new HashSet<String>(), path);
         List<List<Integer>> result = new ArrayList<>();
-        for(List<Integer> connection : connections) {
-            int m = path[connection.get(0)][connection.get(1)] + path[connection.get(1)][connection.get(0)];
-            if (m == 2) {
-                result.add(connection);
-            }
-        }
+        traverse(links, 0, -1, id, visited, low, desc, result);
         return result;
     }
 
-    private void traverse(List<Integer>[] links, int root, Set<String> trace, int[][] path) {
-        for (Integer node : links[root]) {
-            if (trace.contains(root + "_" + node) || trace.contains(node + "_" + root)) continue;
-            trace.add(root + "_" + node);
-            trace.add(node + "_" + root);
-            path[root][node]++;
-            traverse(links, node, new HashSet(trace), path);
+    private int traverse(List<Integer>[] links,
+                         int root,
+                         int parent,
+                         int id,
+                         int[] visited,
+                         int[] low,
+                         int[] desc,
+                         List<List<Integer>> result) {
+        if (visited[root] == 1) return low[root];
+        visited[root] = 1;
+        desc[root] = id;
+        low[root] = id;
+        for (Integer link : links[root]) {
+            if (link == parent) continue;
+            low[root] = Math.min(low[root], traverse(links, link, root, ++id, visited, low, desc, result));
+            if (low[link] > desc[root]) {
+                result.add(List.of(root, link));
+            }
         }
+        return low[root];
     }
 
     private List<Integer>[] link(int n, List<List<Integer>> connections) {
--- src/test/groovy/pw/avvero/leet/year2024_03/ProblemTests1192.groovy
+++ src/test/groovy/pw/avvero/leet/year2024_03/ProblemTests1192.groovy
@@ -11,8 +11,10 @@ class ProblemTests1192 extends Specification {
         expect:
         new Problem1192().criticalConnections(n, connections) == result
         where:
-        n | connections                      || result
-        4 | [[0, 1], [1, 2], [2, 0], [1, 3]] || [[1, 3]]
+        n | connections                                              || result
+        6 | [[0, 1], [1, 2], [2, 0], [1, 3], [3, 4], [4, 5], [5, 3]] || [[1, 3]]
+        4 | [[0, 1], [1, 2], [2, 0], [1, 3]]                         || [[1, 3]]
+        2 | [[0, 1]]                                                 || [[0, 1]]
     }
 
 }
