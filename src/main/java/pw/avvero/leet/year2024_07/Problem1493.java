a26c405590b0e9718060c61eb070625241837fa8
belyaev-ay <anton.belyaev@sibit.tech>
2024-09-04 12:09:44 +0700

    Fix

---
+++ src/main/java/pw/avvero/leet/year2024_07/Problem1493.java
@@ -0,0 +1,30 @@
+package pw.avvero.leet.year2024_07;
+
+public class Problem1493 {
+
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+        int i = 0, j = 0, z = 1;
+        int maxSoFar = 0;
+        while (j < nums.length && i < nums.length) {
+            if (nums[j] == 1) {
+                j++;
+                maxSoFar++;
+            } else {
+                if (z == 1) {
+                    z--;
+                    j++;
+                } else {
+                    if (nums[i] == 0) {
+                        z++;
+                    } else {
+                        maxSoFar--;
+                    }
+                    i++;
+                }
+            }
+            max = Math.max(max, maxSoFar);
+        }
+        return max;
+    }
+}
---
+++ src/test/groovy/pw/avvero/leet/year2024_09/ProblemTests875.groovy
@@ -0,0 +1,20 @@
+package pw.avvero.leet.year2024_09
+
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ProblemTests875 extends Specification {
+
+    @Unroll
+    def "test"() {
+        expect:
+        new Problem875().minEatingSpeed(piles as int[], hours) == result
+        where:
+        piles                | hours || result
+        [1, 1, 1, 999999999] | 10    || 142857143
+        [3, 6, 7, 11]        | 8     || 4
+
+    }
+
+}
