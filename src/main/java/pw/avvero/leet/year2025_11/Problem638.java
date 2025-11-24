bdf7f6d1c6434fe0f0725f92794fe667298c1ad1
Антон Беляев <avvero@mts.ru>
2025-11-24 12:22:50 +0700

    Update

--- src/main/java/pw/avvero/leet/year2025_11/Problem638.java
+++ src/main/java/pw/avvero/leet/year2025_11/Problem638.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class Problem638 {
 
-    private int max = Integer.MAX_VALUE;
+    private int min = Integer.MAX_VALUE;
 
     public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
         track(price, special, needs, 0, 0);
-        return max;
+        return min;
     }
 
     private void track(List<Integer> price, List<List<Integer>> special, List<Integer> needs, int s, int priceSoFar) {
@@ -17,13 +17,13 @@ public class Problem638 {
             if (needs.get(i) < 0) return;
             priceForNeeds += needs.get(i) * price.get(i);
         }
-        max = Math.max(max, priceSoFar + priceForNeeds);
+        min = Math.min(min, priceSoFar + priceForNeeds);
         for (int i = s; i < special.size(); i++) {
             List<Integer> offer = special.get(i);
             //
             List<Integer> remainNeeds = new ArrayList<>();
             boolean belowZero = false;
-            for (int j = 0; j < needs.size(); i++) {
+            for (int j = 0; j < needs.size(); j++) {
                 int v = needs.get(j) - offer.get(j);
                 remainNeeds.add(v);
                 if (v < 0) {
@@ -31,7 +31,7 @@ public class Problem638 {
                     break;
                 }
             }
-            if (belowZero) break;
+            if (belowZero) continue;
             Integer offerPrice = offer.get(offer.size() - 1);
             track(price, special, remainNeeds, i, priceSoFar + offerPrice);
         }
--- src/test/groovy/pw/avvero/leet/year2025_11/ProblemTests638.groovy
+++ src/test/groovy/pw/avvero/leet/year2025_11/ProblemTests638.groovy
@@ -9,6 +9,7 @@ class ProblemTests638 extends Specification {
     @Unroll
     def "test"() {
         expect:
+        new Problem638().shoppingOffers([2,3], [[1,0,1],[0,1,2]], [1,1]) == 3
         new Problem638().shoppingOffers([2, 5], [[3, 0, 5], [1, 2, 10]], [3, 2]) == 14
     }
 
